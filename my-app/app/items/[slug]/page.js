fbdbf394098437d8c16b06af7cecc5bce8453f5d
Sebastian <sjoerz@skyface.de>
2023-08-03 10:06:45 +0200

    dynamic routing

---
+++ my-app/app/items/[slug]/page.js
@@ -0,0 +1,27 @@
+'use client';
+import { useState, useEffect } from 'react';
+
+async function getItem(itemID) {
+  const res = await fetch('http://localhost:8000/api/v1/items/' + itemID);
+  const data = await res.json();
+  return data;
+}
+
+export default function Page({ params }) {
+  const [item, setItem] = useState(null);
+  useEffect(() => {
+    getItem(params.slug).then((item) => {
+      console.log(item);
+      setItem(item);
+    });
+  }, [params.slug]);
+  if (!item) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <p>{item.description}</p>
+    </div>
+  );
+}
