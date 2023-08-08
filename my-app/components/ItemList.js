4a9f0d888886fc4ac5f48522ee80f309fb80c3f8
Marcel <mrcl.k@yahoo.com>
2023-08-08 14:10:03 +0200

    Moved button position on the right side

--- my-app/components/ItemList.js
+++ my-app/components/ItemList.js
@@ -7,12 +7,13 @@ export default function ItemList({ items }) {
   return (
     <div>
       {items.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} style={{display: 'flex', justifyContent: 'center'}}>
+          <div style={{marginRight: '20px'}}>
           <a href={'/items/' + item.id}>
             <h3>{item.id}</h3>
             <p>{item.name}</p>
           </a>
-
+          </div>
           <button
             style={{ color: 'black', padding: '10px 20px', marginBottom: '20px'}}
             onClick={async () => {
--- my-app/components/KistenList.js
+++ my-app/components/KistenList.js
@@ -7,36 +7,38 @@ export default function KistenList({ kisten }) {
   return (
     <div>
       {kisten.map((kiste) => (
-        <div key={kiste.id}>
-          <a href={'/kisten/' + kiste.id}>
-            <h3>{kiste.id}</h3>
-            <p>{kiste.name}</p>
-          </a>
-          <button
-            style={{ color: 'black', padding: '10px 20px', marginBottom: '20px' }}
-            onClick={async () => {
-              fetch(apiURL + '/kisten/' + kiste.id, {
-                method: 'DELETE',
-              })
-                .then((res) => {
-                  if (res.status === 409) {
+        <div key={kiste.id} style={{display: 'flex', justifyContent: 'center'}}>
+          <div style={{marginRight: '20px'}}>
+            <a href={'/kisten/' + kiste.id}>
+              <h3>{kiste.id}</h3>
+              <p>{kiste.name}</p>
+            </a>
+            </div>
+            <button
+              style={{ color: 'black', padding: '10px 20px', marginBottom: '20px' }}
+              onClick={async () => {
+                fetch(apiURL + '/kisten/' + kiste.id, {
+                  method: 'DELETE',
+                })
+                  .then((res) => {
+                    if (res.status === 409) {
+                      alert(
+                        'Error deleting chest. Probably still has items in it.'
+                      );
+                      return;
+                    }
+                    window.location.reload();
+                  })
+                  .catch((err) => {
                     alert(
                       'Error deleting chest. Probably still has items in it.'
                     );
-                    return;
-                  }
-                  window.location.reload();
-                })
-                .catch((err) => {
-                  alert(
-                    'Error deleting chest. Probably still has items in it.'
-                  );
-                });
-            }}
-          >
-            Delete
-          </button>
-        </div>
+                  });
+              }}
+            >
+              Delete
+            </button>
+          </div>
       ))}
     </div>
   );
